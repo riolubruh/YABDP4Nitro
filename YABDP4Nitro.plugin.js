a80070fd98add89c5fb7d447d7231181ca82b763
riolubruh <54255074+riolubruh@users.noreply.github.com>
2024-11-11 14:16:07 -0600

    Bump version

--- YABDP4Nitro.plugin.js
+++ YABDP4Nitro.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name YABDP4Nitro
  * @author Riolubruh
- * @version 5.5.3
+ * @version 5.5.4
  * @invite EFmGEWAUns
  * @source https://github.com/riolubruh/YABDP4Nitro
  * @donate https://github.com/riolubruh/YABDP4Nitro?tab=readme-ov-file#donate
@@ -73,14 +73,14 @@ module.exports = (() => {
 				"discord_id": "359063827091816448",
 				"github_username": "riolubruh"
 			}],
-			"version": "5.5.3",
+			"version": "5.5.4",
 			"description": "Unlock all screensharing modes, and use cross-server & GIF emotes!",
 			"github": "https://github.com/riolubruh/YABDP4Nitro",
 			"github_raw": "https://raw.githubusercontent.com/riolubruh/YABDP4Nitro/main/YABDP4Nitro.plugin.js"
 		},
 		changelog: [
 			{
-				title: "5.5.3",
+				title: "5.5.4",
 				items: [
 					"Override clip experiments since not everybody has it enabled.",
 					"Fixed a problem where the plugin would leave window.global.define as undefined for an extended period of time which other parts of the Discord client sometimes need.",
