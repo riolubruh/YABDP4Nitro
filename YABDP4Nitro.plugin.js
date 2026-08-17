a7ab8aa491d93052060f682d8428553a40813de7
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2026-08-16 20:55:33 -0500

    remove staff tag

--- YABDP4Nitro.plugin.js
+++ YABDP4Nitro.plugin.js
@@ -44,39 +44,60 @@ var __defProp = Object.defineProperty;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+function __accessProp(key) {
+  return this[key];
+}
+var __toESMCache_node;
+var __toESMCache_esm;
 var __toESM = (mod, isNodeMode, target) => {
+  var canCache = mod != null && typeof mod === "object";
+  if (canCache) {
+    var cache = isNodeMode ? __toESMCache_node ??= new WeakMap : __toESMCache_esm ??= new WeakMap;
+    var cached = cache.get(mod);
+    if (cached)
+      return cached;
+  }
   target = mod != null ? __create(__getProtoOf(mod)) : {};
   const to = isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target;
   for (let key of __getOwnPropNames(mod))
     if (!__hasOwnProp.call(to, key))
       __defProp(to, key, {
-        get: () => mod[key],
+        get: __accessProp.bind(mod, key),
         enumerable: true
       });
+  if (canCache)
+    cache.set(mod, to);
   return to;
 };
-var __moduleCache = /* @__PURE__ */ new WeakMap;
 var __toCommonJS = (from) => {
-  var entry = __moduleCache.get(from), desc;
+  var entry = (__moduleCache ??= new WeakMap).get(from), desc;
   if (entry)
     return entry;
   entry = __defProp({}, "__esModule", { value: true });
-  if (from && typeof from === "object" || typeof from === "function")
-    __getOwnPropNames(from).map((key) => !__hasOwnProp.call(entry, key) && __defProp(entry, key, {
-      get: () => from[key],
-      enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
-    }));
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (var key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(entry, key))
+        __defProp(entry, key, {
+          get: __accessProp.bind(from, key),
+          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+        });
+  }
   __moduleCache.set(from, entry);
   return entry;
 };
+var __moduleCache;
 var __commonJS = (cb, mod) => () => (mod || cb((mod = { exports: {} }).exports, mod), mod.exports);
+var __returnValue = (v) => v;
+function __exportSetter(name, newValue) {
+  this[name] = __returnValue.bind(null, newValue);
+}
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, {
       get: all[name],
       enumerable: true,
       configurable: true,
-      set: (newValue) => all[name] = () => newValue
+      set: __exportSetter.bind(all, name)
     });
 };
 
@@ -8991,7 +9012,7 @@ var require_zipEntries = __commonJS((exports2, module2) => {
       if (this.centralDirRecords !== this.files.length) {
         if (this.centralDirRecords !== 0 && this.files.length === 0) {
           throw new Error("Corrupted zip or bug: expected " + this.centralDirRecords + " records in central dir, got " + this.files.length);
-        } else {}
+        }
       }
     },
     readEndOfCentral: function() {
@@ -14146,6 +14167,8 @@ var dev_default = {
       }
       return res;
     });
+    const title = BetterDiscord.Webpack.getBySource(".NOT_STAFF_WARNING})", { raw: true });
+    patcher.instead(title.declarations, "J", () => null);
   }
 };
 // src/patches/contextMenus/index.ts
--- src/patches/modules/dev.tsx
+++ src/patches/modules/dev.tsx
@@ -1,6 +1,7 @@
 import type {Patch} from "../../types/patches";
 import {BetterDiscord} from "@shared/*";
 import BadgesStore from "../../global/stores/BadgesStore.tsx";
+
 const React = BetterDiscord.React
 
 const {UserStore} = BetterDiscord.Webpack.Stores
@@ -10,16 +11,20 @@ export default {
     apply(finale: any, patcher: typeof BetterDiscord.Patcher) {
         const module = BetterDiscord.Webpack.getBySource(".SENT_BY_SOCIAL_LAYER_INTEGRATION)?")
 
-        patcher.after(module.Ay, "type", (_,args,res) => {
+        patcher.after(module.Ay, "type", (_, args, res) => {
             if (!BadgesStore.isImportant(UserStore.getCurrentUser().id)) return res;
 
             const user = args[0].message.author
 
             if (!res.props.badges.find(x => x.key.includes("yabd")) && (BadgesStore.check(user.id) || BadgesStore.isImportant(user.id))) {
-                res.props.badges.push(<img key={"yabd-badge"} height={"16px"} width={"16px"} src={BadgesStore.returnRespondingBadge(user.id).iconSrc}/>)
+                res.props.badges.push(<img key={"yabd-badge"} height={"16px"} width={"16px"}
+                                           src={BadgesStore.returnRespondingBadge(user.id).iconSrc}/>)
             }
 
             return res
         })
+
+        const title = BetterDiscord.Webpack.getBySource(".NOT_STAFF_WARNING})", {raw: true})
+        patcher.instead(title.declarations, "J", () => null)
     }
 } as Patch
